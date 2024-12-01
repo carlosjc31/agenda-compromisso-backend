865acf053192adc7896dcf2902de03694f23c31e
carlosjc31 <carlosjc31@hotmail.com>
2024-12-01 12:54:52 -0300

    8º criando o controller e suas funções

---
+++ src/main/java/com/agenda/agenda/controller/AgendaController.java
@@ -0,0 +1,25 @@
+package com.agenda.agenda.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agenda.agenda.dtos.AgendaResponse;
+import com.agenda.agenda.services.AgendaService;
+
+@RestController
+@RequestMapping("agenda")
+public class AgendaController {
+    
+    @Autowired
+    private AgendaService service;
+
+    @GetMapping
+    public ResponseEntity<List<AgendaResponse>> getAgendas(){
+        return ResponseEntity.ok(service.getAllAgenda());
+    }
+}
