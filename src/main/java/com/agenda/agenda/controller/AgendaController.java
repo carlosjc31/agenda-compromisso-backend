80f87c08c3fc245d8f675baf388b2705e62fc620
carlosjc31 <carlosjc31@hotmail.com>
2024-12-01 13:10:55 -0300

    10º criando responseEntity no controller e service

--- src/main/java/com/agenda/agenda/controller/AgendaController.java
+++ src/main/java/com/agenda/agenda/controller/AgendaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,10 @@ public class AgendaController {
     public ResponseEntity<List<AgendaResponse>> getAgendas(){
         return ResponseEntity.ok(service.getAllAgenda());
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AgendaResponse> getAgenda(@PathVariable Long id){
+        return ResponseEntity.ok(service.getAgendaById(id));
+    }
+    
 }
--- src/main/java/com/agenda/agenda/services/AgendaService.java
+++ src/main/java/com/agenda/agenda/services/AgendaService.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.agenda.agenda.dtos.AgendaResponse;
+import com.agenda.agenda.entities.Agenda;
 import com.agenda.agenda.mappers.AgendaMapper;
 import com.agenda.agenda.repositories.AgendaRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AgendaService {
@@ -23,4 +26,10 @@ public class AgendaService {
                          .collect(Collectors.toList());
     }
     
+    public AgendaResponse getAgendaById(Long id){
+        Agenda agenda = repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Agenda não encontrada")
+        );
+        return AgendaMapper.toDTO(agenda);
+    }
 }
