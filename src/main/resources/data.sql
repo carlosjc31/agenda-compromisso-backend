9e2d9bde3a8115bb6f53e861659aa0d72497bae1
carlosjc31 <carlosjc31@hotmail.com>
2024-12-01 13:00:56 -0300

    9º corrigindo as entidade date no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,14 @@
 INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
-VALUES('Prova estatistica', 'Prova de estatistica', '2024-12-02', 'Lab. 4');
+VALUES('Prova estatistica', 'Prova de estatistica', '2024-12-02 08:00:00', 'Lab. 4');
 
 INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
-VALUES('Prova Web', 'Prova de programação web utilizando Spring e Java', '2024-12-03', 'Lab. 6');
+VALUES('Prova Web', 'Prova de programação web utilizando Spring e Java', '2024-12-03 08:00:00', 'Lab. 6');
 
 INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
-VALUES('Trabalho de Eng. de software 2', 'Entrega de trabalho de engenharia de software 2', '2024-12-05', 'Lab. 4');
+VALUES('Trabalho de Eng. de software 2', 'Entrega de trabalho de engenharia de software 2', '2024-12-05 08:00:00', 'Lab. 4');
 
 INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
-VALUES('entrega do trabalho', 'Entrega de trabalho Web, fazendo deploy na AWS', '2024-12-10', 'Lab. 6');
+VALUES('entrega do trabalho', 'Entrega de trabalho Web, fazendo deploy na AWS', '2024-12-10 08:00:00', 'Lab. 6');
+
+INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
+VALUES('Entrega do trabalho', 'Entrega de trabalho SO 2', '2024-12-04 09:30:00', 'Lab. 6');
