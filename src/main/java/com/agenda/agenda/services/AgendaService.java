503df795ee4c4b486a154c45a1918afc934cfd25
carlosjc31 <carlosjc31@hotmail.com>
2024-12-01 12:48:25 -0300

    7º criando uma lista no service

--- src/main/java/com/agenda/agenda/services/AgendaService.java
+++ src/main/java/com/agenda/agenda/services/AgendaService.java
@@ -1,8 +1,26 @@
 package com.agenda.agenda.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.agenda.agenda.dtos.AgendaResponse;
+import com.agenda.agenda.mappers.AgendaMapper;
+import com.agenda.agenda.repositories.AgendaRepository;
 
 @Service
 public class AgendaService {
+
+    @Autowired
+    private AgendaRepository repository;
+
+    public List<AgendaResponse> getAllAgenda(){
+        return repository.findAll()
+                         .stream()
+                         .map(a -> AgendaMapper.toDTO(a))
+                         .collect(Collectors.toList());
+    }
     
 }
