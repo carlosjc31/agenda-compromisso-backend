a227f4d3de34e0a3c6a940f17b42d38b22bc5a5b
carlosjc31 <carlosjc31@hotmail.com>
2024-12-01 11:08:29 -0300

    5º inserindo atributos no no BD h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
+VALUES('Prova estatistica', 'Prova de estatistica', '2024-12-02', 'Lab. 4');
+
+INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
+VALUES('Prova Web', 'Prova de programação web utilizando Spring e Java', '2024-12-03', 'Lab. 6');
+
+INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
+VALUES('Trabalho de Eng. de software 2', 'Entrega de trabalho de engenharia de software 2', '2024-12-05', 'Lab. 4');
+
+INSERT INTO AGENDA (TITLE, DESCRIPTION, DATE, LOCAL)
+VALUES('entrega do trabalho', 'Entrega de trabalho Web, fazendo deploy na AWS', '2024-12-10', 'Lab. 6');
