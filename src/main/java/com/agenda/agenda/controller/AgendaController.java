daba8d8c7d19624d8832f08b62baa319067c6f50
carlosjc31 <carlosjc31@hotmail.com>
2024-12-12 13:59:04 -0300

    correção da versão

--- src/main/java/com/agenda/agenda/controller/AgendaController.java
+++ src/main/java/com/agenda/agenda/controller/AgendaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.agenda.agenda.dtos.AgendaRequest;
 import com.agenda.agenda.dtos.AgendaResponse;
 import com.agenda.agenda.services.AgendaService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("agenda")
 public class AgendaController {
